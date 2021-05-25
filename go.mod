c0ef39b2d87c50039725da9262d4f6233fdc4100
karl.runge <karl.runge@defendtex.com>
2021-05-25 22:57:53 +1000

    Completed generation

--- go.mod
+++
@@ -1,5 +0,0 @@
-module github.com/IAmMadfly/random_word_file_generator
-
-go 1.15
-
-require github.com/sendgrid/rest v2.6.4+incompatible
--- main.go
+++ main.go
@@ -1,32 +1,65 @@
 package main
 
 import (
+	"bufio"
 	"encoding/json"
 	"fmt"
+	"math/rand"
+	"os"
 	"strconv"
 	"sync"
 
+	"github.com/alexflint/go-arg"
 	"github.com/sendgrid/rest"
 )
 
-func write_to_file(file_name string, word_pool *[]string, waiter *sync.WaitGroup) {
-	// file, err := os.Create("./Data_0.txt")
+func write_to_file(
+	file_name string, 
+	word_pool *[]string, 
+	waiter *sync.WaitGroup, 
+	word_count int,
+	) {
+	file, err := os.Create(file_name)
 
-	// if err != nil {
-	// 	fmt.Println("Failed to make file!")
-	// 	return
-	// }
+	if err != nil {
+		fmt.Println("Failed to make file!")
+		return
+	}
 
 	// writer := bufio.NewWriter(file)
+	writer := bufio.NewWriterSize(file, 10000)
+	var rand_index int
+	for i := 0; i < word_count; i++ {
+		rand_index = rand.Intn(len(*word_pool))
+		writer.Write([]byte((*word_pool)[rand_index]+"\n"))
+	}
 
-	fmt.Println(len(*word_pool))
+	fmt.Println(file_name + "Completed writing")
+	writer.Flush()
 	waiter.Done()
 }
 
 func main() {
 
-	var word_pool int
-	word_pool = 10
+	var args struct {
+		Pool int
+		Count int
+		Files int
+	}
+	args.Pool = 1000
+	args.Count = 1000
+	args.Files = 10
+
+	arg.MustParse(&args)
+
+	fmt.Println(
+		"Word pool:", args.Pool, 
+		", Words per file:", args.Count, 
+		", File count:", args.Files,
+	)
+
+	word_pool := args.Pool
+	word_count := args.Count
 
 	const host = "https://random-word-api.herokuapp.com/word"
 	param := "number"
@@ -54,15 +87,15 @@ func main() {
 		fmt.Printf("Response: %s\n", words[0])
 	}
 
-	// linesToWrite := []string{"some kind", "of data to be", "Written to file"}
-
-	// writer.WriteString(linesToWrite[0])
 	var wait_group sync.WaitGroup
-	for i := 0; i < 10; i++ {
+	fmt.Println("Starting to write random word files!")
+	for i := 0; i < args.Files; i++ {
+		file_name := "data_"+strconv.Itoa(i)+".txt"
 
 		wait_group.Add(1)
-		go write_to_file("data_"+strconv.Itoa(i), &words, &wait_group)
+		go write_to_file(file_name, &words, &wait_group, word_count)
 	}
 
 	wait_group.Wait()
+	fmt.Println("Task Complete!")
 }
